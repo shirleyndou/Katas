f30539a528c2ac50d2907abcc299d53ae097240d
shirleyndou <shirley.ndou@umuzi.org>
2020-07-01 15:51:58 +0200

    Even or Odd function initial commit

---
+++ evenOrOdd.js
@@ -0,0 +1,9 @@
+function evenOrOdd(num) {
+    if (num % 2 == 0) {
+        console.log("Number is even");
+    }
+    else {
+        console.log("Number is odd");
+    }
+}
+evenOrOdd(25);
---
+++ evenOrOdd.ts
@@ -0,0 +1,11 @@
+function evenOrOdd(num: number){
+    if(num%2 ==0){
+        console.log("Number is even");
+    }
+    else{
+        console.log("Number is odd");
+    }
+
+}
+
+evenOrOdd(25);
