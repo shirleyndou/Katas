2a8edba991bd0b241c76cf4df4256a44e3817d71
shirleyndou <shirley.ndou@umuzi.org>
2020-07-03 14:27:54 +0200

    drawing a square initial commit

---
+++ square.js
@@ -0,0 +1,13 @@
+var printout = "";
+function square(sqNums) {
+    for (var row = 1; row <= sqNums; row++) {
+        for (var col = 1; col <= sqNums; col++) {
+            //console.log("#");
+            printout += "#";
+        }
+        //console.log("\n")
+        printout += "\n";
+    }
+    console.log(printout);
+}
+square(2);
---
+++ square.ts
@@ -0,0 +1,14 @@
+let printout : string = "";
+function square(sqNums: number){
+    for(let row=1; row<=sqNums; row++){
+        for(let col=1; col<=sqNums; col++){
+            //console.log("#");
+            printout += "#"
+        }
+        //console.log("\n")
+        printout += "\n"
+    }
+    console.log(printout);
+}
+
+square(2);
